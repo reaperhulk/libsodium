ed81ea4a5ec6a63d2189c21e9bb5caca6e31b0ee
Paul Kehrer <paul.l.kehrer@gmail.com>
2019-01-15 10:40:33 -0600

    oss-fuzz based fuzzers

---
+++ fuzz/vrf_fuzzer.cc
@@ -0,0 +1,37 @@
+#include <string.h>
+#include <sodium/crypto_vrf.h>
+#include <sodium.h>
+#include <stdio.h>
+#include <assert.h>
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+    int ret;
+
+    ret = sodium_init();
+    assert(ret >= 0);
+    unsigned char pk[crypto_vrf_PUBLICKEYBYTES];
+    unsigned char sk[crypto_vrf_SECRETKEYBYTES];
+    unsigned char seed[crypto_vrf_SEEDBYTES] = {};
+    // copy as many bytes into the seed as we can
+    int seedsize = crypto_vrf_SEEDBYTES;
+    if (size < crypto_vrf_SEEDBYTES) {
+        seedsize = size;
+    }
+    memcpy(seed, data, seedsize);
+    crypto_vrf_keypair_from_seed(pk, sk, seed);
+    ret = crypto_vrf_is_valid_key(pk);
+
+    unsigned char proof[crypto_vrf_PROOFBYTES];
+    int proofsize = crypto_vrf_PROOFBYTES;
+    if (size < crypto_vrf_PROOFBYTES) {
+        proofsize = size;
+    }
+    memcpy(proof, data, proofsize);
+
+    unsigned char output[crypto_vrf_OUTPUTBYTES];
+    ret = crypto_vrf_proof_to_hash(output, proof);
+
+    unsigned char voutput[crypto_vrf_OUTPUTBYTES];
+    ret = crypto_vrf_verify(voutput, pk, proof, data, size);
+    return 0;
+}
